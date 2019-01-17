57ba4be5e84734ec78d548eb30306493941b51e3
kohjunhao <kohjunhao0119@gmail.com>
2019-01-18 02:32:39 +0800

    ,

--- main.py
+++ main.py
@@ -46,7 +46,7 @@ async def on_message(message):
         
         
     if message.content.lower().startswith('YEETSKETIT'):
-        msg = 'fuck off nigga {0.author.mention}'.format(message)
+        msg = 'fuck off nigga, {0.author.mention}'.format(message)
         await client.send_message(message.channel,msg)
 
 
