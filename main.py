af8755b682262df633bda068fc075a9592e0ff91
kohjunhao <kohjunhao0119@gmail.com>
2019-01-18 03:14:01 +0800

    a

--- main.py
+++ main.py
@@ -66,7 +66,7 @@ async def on_message(message):
         
         await client.send_message(message.channel,msg)
 
-    if message.content.lower().startswith('!!math'):
+    if message.content.lower().startswith('!!choose'):
         msg = msg.split('|')
         length = len(msg)
         choice = random.randint(0,length-1)
